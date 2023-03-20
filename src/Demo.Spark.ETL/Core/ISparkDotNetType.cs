352a739315d2a9d80705e22775ee2549e89f6d1e
Cheranga <cheranga@gmail.com>
2023-03-21 10:05:40 +1100

    fix: removing unwanted properties from data type definitions [ci]

--- src/Demo.Spark.ETL/Core/ISparkDotNetType.cs
+++ src/Demo.Spark.ETL/Core/ISparkDotNetType.cs
@@ -10,13 +10,9 @@ public interface ISparkDotNetType<out TSpark, out TDotNet> where TSpark:DataType
 [ExcludeFromCodeCoverage]
 public abstract class StringDataType : ISparkDotNetType<StringType, string>
 {
-    public StringType SparkType => new();
-    public string DotNetType => string.Empty;
 }
 
 [ExcludeFromCodeCoverage]
 public abstract class IntegerDataType : ISparkDotNetType<IntegerType, int>
 {
-    public IntegerType SparkType => new();
-    public int DotNetType => 666;
 }
